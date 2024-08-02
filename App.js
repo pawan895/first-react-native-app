d41762cf9139c46721cacede771a1cbfb293bb1a
pawan895 <81580980+pawan895@users.noreply.github.com>
2024-08-02 15:32:41 +0530

    First react native app

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style ={styles.text}>Aarogya bhoomi</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 13.3702,
+          longitude: 77.6835,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 13.3702,
+          longitude: 77.6835,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +36,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: '80%',
+    height: '40%', 
+    padding: 20,
+    margin: 20, 
+    // Adjust height as needed
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 20,
+  }
 });
